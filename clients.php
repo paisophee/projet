9bb01e884ad7759498677b92a23bd314b85ca31f
Marie Ullmansberger <Marie Ullmansberger>
2019-03-07 12:19:58 +0100

    Finalisation du projet

---
+++ clients.php
@@ -0,0 +1,5 @@
+<?php
+
+// Projet
+
+echo "Mon projet";
---
+++ fournisseurs.php
@@ -0,0 +1,13 @@
+<?php
+
+// Projet
+
+echo "Mon projet";
+
+// Mise à jour
+
+?>
+<a href="page.php">Lien vers la page</a>;
+<a href="clients.php">Clients</a>;
+<a href="fournisseurs.php">Clients</a>;
+<?php 
